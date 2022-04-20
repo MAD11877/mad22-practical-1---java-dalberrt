f148ab4f60200b5722d03b28c9c74a293f5979c5
dalberrt <dalbertchea@gmail.com>
2022-04-20 09:09:05 +0800

    Update Question 2

--- Question2.java
+++ Question2.java
@@ -11,13 +11,11 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter your height(m): ");
     double height = in.nextDouble();
 
-    System.out.print("Enter your weight(kg): ");
     double weight = in.nextDouble();
     double bmi = weight / (height*height);
-    System.out.print("Your BMI is: " + bmi);
+    System.out.print(bmi);
 
     in.close();
   }
